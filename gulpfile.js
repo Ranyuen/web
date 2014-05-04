b92dd854d8e3259ad76e6cf885df567fd1898a6d
ne_Sachirou <utakata.c4se@gmail.com>
2014-05-04 21:17:04 +0900

    Move rake test:test_grunt & test:test_php to Gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,27 @@
 'use strict';
-var gulp = require('gulp'),
+var cp = require('child_process');
+var exec = require('gulp-exec'),
+    gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     jshintStylish = require('jshint-stylish'),
-    less = require('gulp-less');
+    less = require('gulp-less'),
+    Promise = require('bluebird');
+
+/**
+ * @param {string} cmd
+ * @return {Promise.<string>}
+ */
+function childPromise(cmd) {
+  return new Promise(function (resolve, reject) {
+    cp.exec(cmd, function (err, stdout, stderr) {
+      if (err) {
+        process.stderr.write(stderr);
+        return reject(err);
+      }
+      resolve(stdout);
+    });
+  });
+}
 
 gulp.task('jshint', function () {
   gulp.src(['*.js', 'assets/javascripts/*.js']).
@@ -19,5 +38,27 @@ gulp.task('less', function () {
     pipe(gulp.dest('assets/stylesheets'));
 });
 
+gulp.task('php-fixer', function () {
+  Promise.all(['lib/'].map(function (path) {
+    return childPromise('php php-cs-fixer.phar fix ' + path + ' --level=all');
+  })).then(function (stdouts) {
+    stdouts.forEach(function (stdout) { console.log(stdout); });
+  });
+});
+
+gulp.task('php-lint', function () {
+  gulp.src(['lib/**/**.php', 'test/**/**.php']).
+    pipe(exec('php -l <%= file.path %>', {})).
+    pipe(exec.reporter({}));
+});
+
+gulp.task('php-unit', function (done) {
+  cp.exec('vendor/bin/phpunit test', function (err, stdout, stderr) {
+    console.log(stdout);
+    process.stderr.write(stderr);
+    done(err);
+  });
+});
+
 gulp.task('build', ['less']);
-gulp.task('test', ['jshint']);
+gulp.task('test', ['jshint', 'php-lint', 'php-fixer', 'php-unit']);
