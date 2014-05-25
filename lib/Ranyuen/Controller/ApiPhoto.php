0978547cdd89bf795f6b5a475a3945a8367b03cd
ne_Sachirou <utakata.c4se@gmail.com>
2014-05-25 18:22:59 +0900

    Fix #36

--- lib/Ranyuen/Controller/ApiPhoto.php
+++ lib/Ranyuen/Controller/ApiPhoto.php
@@ -1,22 +1,23 @@
 <?php
 namespace Ranyuen\Controller;
 
-class ApiPhoto 
+class ApiPhoto
 {
-	public function render($method, array $uri_params, array $request_params)
+    public function render($method, array $uri_params, array $request_params)
     {
-    	if (!isset($request_params['id'])) {
-    		return [ 'error' => 'id is required.', 'status' => 404 ];
-    	}
-		$filename = 'Calanthe/gallery/' . $request_params['id'] . '.jpg';
-		list($width, $height) = getimagesize($filename);
-		$new_width = 349;
-		$new_height = $height * ($new_width / $width);
-		$image_p = imagecreatetruecolor($new_width, $new_height);
-		$image = imagecreatefromjpeg($filename);
-		imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
-		header('Content-Type: image/jpeg');
-
-		return imagejpeg($image_p, null, 100);
-	}
+        if (!isset($request_params['id'])) {
+            return [ 'error' => 'id is required.', 'status' => 404 ];
+        }
+        $filename = "Calanthe/gallery/{$request_params['id']}.jpg";
+        $image = imagecreatefromjpeg($filename);
+        list($width, $height) = getimagesize($filename);
+        $mini_width = 349;
+        $mini_height = floor($height * $mini_width / $width);
+        $mini_image = imagecreatetruecolor($mini_width, $mini_height);
+        imagecopyresampled($mini_image, $image,
+            0, 0, 0, 0,
+            $mini_width, $mini_height, $width, $height);
+        header('Content-Type: image/jpeg');
+        imagejpeg($mini_image, null, 100);
+    }
 }
