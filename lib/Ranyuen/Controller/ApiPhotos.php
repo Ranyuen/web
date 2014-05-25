8a4562c4511adc5e0b599f766e51c0539d6fe68c
ne_Sachirou <utakata.c4se@gmail.com>
2014-05-26 05:24:28 +0900

    Trap errors.

--- lib/Ranyuen/Controller/ApiPhotos.php
+++ lib/Ranyuen/Controller/ApiPhotos.php
@@ -9,9 +9,15 @@ class ApiPhotos implements ApiController
     {
         $limit = isset($request_params['limit']) ? $request_params['limit'] : 100;
         $offset = isset($request_params['offset']) ? $request_params['offset'] : 0;
+        $result = [];
+        try {
+            $result = Photo::rawQuery(
+                'select * from photo order by random() limit :limit offset :offset',
+                ['limit' => $limit, 'offset' => $offset]
+            )->findMany();
+        } catch (PDOException $e) {
+        }
 
-        return array_map(function ($photo) {
-            return $photo->asArray();
-        }, Photo::rawQuery('select * from photo order by random() limit :limit offset :offset', ['limit' => $limit, 'offset' => $offset])->findMany());
+        return array_map(function ($photo) { return $photo->asArray(); }, $result);
     }
 }
