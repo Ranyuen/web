180ca2a020ea58b333b83568353f583f15c2efdb
midoribito <jingaimidoribito@gmail.com>
2014-07-20 21:23:20 +0900

    Side menu is shitgit commit

--- templates/layout.php
+++ templates/layout.php
@@ -58,53 +58,19 @@ $switch_lang = implode(' / ', $switch_lang);
     </header>
     <!-- <div id="header-container-separator"> </div> -->
     <div class="container">
-        <!-- <div id="indicate">0</div>
-        <script>
-        var t = 0;
-        function rfrsh()
-        {
-            if (t % 10 === 0) {
-                t = 0;
-                document.getElementById('indicate').textContent =
-                    window.getComputedStyle(document.querySelector('.container')).width + '/' + window.getComputedStyle(document.body).width;
-            }
-            ++ t;
-            requestAnimationFrame(rfrsh);
-        }
-        requestAnimationFrame(rfrsh);
-        </script> -->
-        <article>
-            <header>
-                <!-- <div class="aside">
-                    <div class="news">
-                        <img src="/assets/images/icons/feed.png"
-                             width="30" height="30"/>NEWS
-                    <?php $h->echoNav($news_nav, "{$link['base']}/news/"); ?>
-                    </div>
-                    <a href="https://www.facebook.com/ranyuenjapan"
-                       target="_blank"
-                       class="facebook"
-                       title="Ranyuen Japan Facebook Page">
-                        <img src="/assets/images/icons/facebook.png"
-                             alt="Ranyuen Japan Facebook Page"
-                             longdesc="https://www.facebook.com/ranyuenjapan"/>
-                        <div style="line-height:13pt;">Ranyuen<br/>Facebook</div>
-                    </a>
-                </div> -->
-                <?php $h->echoBreadcrumb($breadcrumb, $link['base']); ?>
-            </header>
+        <div id="main">
+            <div id="mainInner">
+            <?php $h->echoBreadcrumb($breadcrumb, $link['base']); ?>
             <article><?php $h->render($content, $__params); ?></article>
-        </article>
-        <footer>
+            </div>
+            <div id="sidebar">
             <nav><?php $h->echoNav($global_nav, $link['base']); ?></nav>
+            </div>
+        </div>
+        <footer>
             <p class="copyright">Copyright (C) 2010-2014 <a rel="home" href="<?php $h->h($home); ?>">Ranyuen</a> All Rights Reserved.<br/>
             Spring Calanthe (EBINE) and Ponerorchis (AWACHIDORI &amp; YUMECHIDORI) you see on our website are all bred, researched and developed in our <a rel="home" href="<?php $h->h($home); ?>">Ranyuen</a>&#39;s farm.</p>
         </footer>
-    </div>
-    <div id="sidebar">
-        <nav class="local-nav">
-            <?php $h->echoNav($local_nav, $local_base); ?>
-        </nav>
     </div>
     <script>
     window.addEventListener('DOMContentLoaded', function () {
--- templates/news/index.ja.markdown
+++ templates/news/index.ja.markdown
@@ -1,4 +1,17 @@
 ---
 title: お知らせ
 ---
+<style>
+h1 {
+  background: red;
+  color: white;
+  display: block;
+  height: 40px;
+  line-height: 40px;
+  margin: 0.5em 0;
+  text-align: center;
+  width: 100%;
+}
+</style>
+
 <?php $h->echoNav($news_nav, "{$link['base']}/news/"); ?>
