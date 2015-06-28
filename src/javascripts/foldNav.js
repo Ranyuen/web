439cbed695b31df133b386bc9c78667f52b46147
truthNH <truth.nh@hotmail.co.jp>
2015-06-28 20:19:47 +0900

    Modify サイドナビゲーションの折りたたみ

--- src/javascripts/foldNav.js
+++ src/javascripts/foldNav.js
@@ -24,15 +24,24 @@
    * @param {HTMLLiElement} item
    */
   function setupItem(item) {
+
+    var i = 0,
+        iz = 0;
+
     item.classList.add('foldnav_item');
     if (item.querySelector('ul')) {
       item.classList.add('foldnav_item-hasSubmenu');
       item.classList.add('foldnav_item-isClose');
       item.addEventListener('click', onClickItemHasSubmenu, true);
       Array.from(item.querySelector('ul').children).filter(function (node) {
+        ++i;
         return 'li' === node.tagName.toLowerCase();
       }).forEach(function (li) {
+        iz++;
         setupItem(li);
+        if (i === iz) {
+          li.classList.add('last_child');
+        }
       });
     }
   }
