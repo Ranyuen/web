d6201090d9bc5bc77e507d9adc840aaebbc41933
midoribito <jingaimidoribito@gmail.com>
2016-05-28 16:13:04 +0900

    アクティブページのローカルメニュー色付け

--- src/javascripts/activeTab.js
+++ src/javascripts/activeTab.js
@@ -11,13 +11,15 @@ $(function () {
     }
   });
   $('#sidenav ul li a').each(function () {
-    var href = $(this).attr('href');
-    if (location.href.match(href)) {
-      if (href !== '/') {
-        $(this).addClass('activate');
-      }
-    } else {
-      $(this).removeClass('activate');
+    // 現在のページのドメイン名を取得
+    var dname = location.href.match(/^https?:\/\/[^\/]+/);
+    // ローカルナビのURLを取得しドメイン名と結合
+    var dhref = dname + $(this).attr('href');
+    // 現在のページのURLを取得
+    var lhref = location.href;
+    // 比較し一致する場合にactivateクラスを付加
+    if (dhref === lhref) {
+      $(this).addClass('activate');
     }
   });
 });
