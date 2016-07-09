c132eda8f0643d4c4c3b3fc27ab70525883d0a4d
truthNH <truth.nh@hotmail.co.jp>
2016-07-09 17:53:33 +0900

    Add test for registering exam_questions and exam_answers

---
+++ tests/ExamTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use Ranyuen\Model\ExamQuestion;
+
+use Illuminate\Database\Capsule\Manager as DB;
+
+class ExamTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp() {
+        $this->configureDatabase();
+    }
+
+    protected function configureDatabase() {
+        $db = new DB;
+        $db->addConnection(array(
+            'host'      => 'localhost',
+            'driver'    => 'mysql',
+            'database'  => 'ranyuen_production',
+            'username'  => 'root',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+        ));
+        $db->bootEloquent();
+        $db->setAsGlobal();
+    }
+
+    public function testReadJSON() {
+        ExamQuestion::readFromJSON('assets/exam/easy.json');
+    }
+}
