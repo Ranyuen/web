9c42e3558ff7f659dfcc4f19b1bd12b8e63571c4
truthNH <truth.nh@hotmail.co.jp>
2016-07-09 17:52:01 +0900

    Add Models of ExamAnswer and ExamQuestion

---
+++ lib/Ranyuen/Model/ExamQuestion.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Ranyuen\Model;
+
+use Illuminate\Database\Eloquent;
+
+/**
+ * ExamQuestion model.
+ */
+class ExamQuestion extends Eloquent\Model
+{
+    protected $table   = 'exam_question';
+    protected $guarded = ['id'];
+
+    public function answers() {
+
+        return $this->hasMany(ExamAnswer::class);
+    }
+
+    public static function readFromJSON($json_file) {
+        if (isset($json_file)) {
+            $exams = json_decode(file_get_contents($json_file), true);
+            // select type easy, hard, expert
+            if ($exams['name'] === '蘭検定 (初級)') {
+                $type = 'easy';
+            } else if ($exam['name'] === '蘭検定 (上級)') {
+                $type = 'hard';
+            } else if ($exam['name'] === '蘭検定 (博士編)') {
+                $type = 'expert';
+            }
+            $exams = $exams['questions'];
+            // register question
+            foreach ($exams as $exam) {
+                foreach ($exam['answers'] as $answer) {
+                    $q = new ExamQuestion(
+                        [
+                            'question' => $exam['q'],
+                            'type'     => $type
+                        ]
+                    );
+                    $q->save();
+                    ExamAnswer::registerExamAnswer($q->id, $answer);
+                }
+            }
+
+            return $exams;
+        }
+    }
+}
