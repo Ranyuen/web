d61f31076c303dbff97429fe53fefc06028442ee
SuguruNakahara <subaru25.net@gmail.com>
2018-11-17 14:54:15 +0900

    add 50 Question with exam

--- bin/readFromJson.php
+++ bin/readFromJson.php
@@ -14,4 +14,5 @@ use Ranyuen\Model\ExamQuestion;
 // ExamQuestion::readFromJSON('assets/exam/20161224_photo.json');
 // ExamQuestion::readFromJSON('assets/exam/20170311_photo.json');
 // ExamQuestion::readFromJSON('assets/exam/20170311_2_photo.json');
-ExamQuestion::readFromJSON('assets/exam/20180929_photo.json');
+// ExamQuestion::readFromJSON('assets/exam/20180929_photo.json');
+ExamQuestion::readFromJSON('assets/exam/20181117_photo.json');
