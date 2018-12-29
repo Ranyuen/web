2832693674e4c10bc6dc83b361547100652a6bdb
SuguruNakahara <subaru25.net@gmail.com>
2018-12-29 19:27:18 +0900

    SideNav exam fix

--- src/javascripts/activeTab.js
+++ src/javascripts/activeTab.js
@@ -22,4 +22,14 @@ $(function () {
       $(this).addClass('activate');
     }
   });
+  // change sideNav the exam ( Exception )
+  $('#sidenav ul li a').each(function () {
+    if($(this).attr('href') == "/play/exam/") {
+      var addElement = $(this)[0].parentNode;
+      var firstNav = $('#sidenav ul li')[0];
+      var removeElement = firstNav.nextElementSibling;
+      removeElement.parentNode.removeChild(removeElement);
+      firstNav.parentNode.insertBefore(addElement, firstNav.nextSibling);
+    }
+  });
 });
